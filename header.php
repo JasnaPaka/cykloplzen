e955fd1704efb74737b965b36eb5eb2398d137c6
Pavel Cvrcek <jasnapaka@jasnapaka.com>
2014-08-17 00:42:09 +0200

    Přidání sidebaru
    Přidání skrývatelného sidebaru na titulce mapy.

--- header.php
+++ header.php
@@ -7,9 +7,14 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	
 	<title><?php wp_title("&raquo;", true, "right"); ?> Cyklomapa Plzně a okolí</title>
-
+	
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+	<link rel="stylesheet" media="(max-width: 500px)" href="<?php bloginfo('template_url'); ?>/style-500.css" />
+	
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/print.css" media="print" />
-	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/leaflet-0.7.3/leaflet.css" />
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/leaflet-0.7.3/leaflet.css" />
+	
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600&subset=latin,cyrillic-ext' rel='stylesheet' type='text/css' />
+	<link rel="icon" type="image/x-icon" href="<?php bloginfo('template_url'); ?>/favicon.ico" />
 </head>
 <body>
--- index.php
+++ index.php
@@ -2,6 +2,58 @@
 
 <?php if (is_home()) { ?>
 <body>
+	<script type="text/javascript">
+		function toggleSidebar() {
+			var sidebar = document.getElementById("legenda");
+			var buttonHide = document.getElementById("button-hide");
+			
+			var style = window.getComputedStyle(sidebar);
+			var buttonStyle = window.getComputedStyle(buttonHide);
+			
+			if (style.getPropertyValue("display") == "block") {
+				sidebar.style.display = "none";
+				buttonHide.style.right = 0;
+				buttonHide.title = "Zobrazí postranní lištu";
+				buttonHide.firstChild.data= "<";
+			} else {
+				sidebar.style.display = "block";
+				buttonHide.style.right = "280px";
+				buttonHide.title = "Skryje postranní lištu";
+				buttonHide.firstChild.data= ">";
+			}
+			
+			L.Util.requestAnimFrame(map.invalidateSize, map, false, map._container);
+		}
+	</script>
+
+	<button id="button-hide" title="Skryje postranní lištu" onclick="toggleSidebar()">&gt;</button>
+
+	<div id="legenda">
+		<img src="<?php bloginfo('template_url'); ?>/images/bicycle-icon.png" alt="Ikona kola" id="logo" />
+	
+		<h1>Cyklomapa Plzně</h1>
+	
+		<div id="legenda-obsah">
+			<p>Komunitní cyklomapa Plzně a okolí, která vzniká v rámci podnětu 
+				programu <a href="http://www.verejnyprostorvplzni.cz/pestuj-prostor">Pěstuj prostor</a> pod
+				<a href="http://plzen2015.cz/">Plzeň 2015</a>.</p>
+				
+			<p>Staví na projektu <a href="http://www.openstreetmap.org/">OpenStreetMap</a> a využívá vzhledu,
+			který vychází z <a href="http://mtbmap.cz/">MTBMap</a> a pro potřeby cyklistiky jej upravilo
+			<a href="http://prahounakole.cz/">Prahou na kole</a>.</p>
+			
+			<h2>Jak se zapojit</h2>
+			
+			<p>Mapa vzniká díky práci dobrovolníků, mezi které se můžete zařadit i vy. Našli jste chybu?
+			Chcete mapu něčím vylepšit? <a href="mailto:jasnapaka@jasnapaka.com">Napište nám</a> nebo
+			si přečtěte, <a href="./mapovani/">jak pomoci s vylepšování mapy</a>.</p>
+			
+			<h2>Podpora</h2>
+			
+			<p>Děkujeme všem, kteří nás podporují. Jmenovitě pak Plzeň 2015, Prahou na kole a <a href="./partneri/">další partneři</a>.</p>
+		</div>
+	</div>
+
 	<div id="map"></div>
 
 	<script src="<?php bloginfo('template_url'); ?>/leaflet-0.7.3/leaflet.js"></script>
