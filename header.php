47d2772ee69d0342b60a900905e24b2200ebf796
Pavel Cvrcek <jasnapaka@jasnapaka.com>
2014-09-14 13:26:05 +0200

    Vylepšení klíčových slov + obr pro FB.

--- header.php
+++ header.php
@@ -2,11 +2,13 @@
 <html lang="cs">
 <head>
 	<meta charset="utf-8" />
-	<meta name="description" content="Komunitní cyklomapa Plzně a okolí" />
-	<meta name="keywords" content="cyklo,kolo,mapa,cyklistika" />
+	<meta name="description" content="Komunitní cyklomapa Plzně a okolí. Přehled oficiálních i neoficiálních cyklostezek a cyklotras." />
+	<meta name="keywords" content="cyklostezky,cyklotrasy,plzeň,cyklo,kolo,mapa,cyklistika" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	
-	<title><?php wp_title("&raquo;", true, "right"); ?> Cyklomapa Plzně a okolí</title>
+	<meta property="og:image" content="http://cyklomapa.plzne.cz/wp-content/themes/cykloplzen/images/bicycle-icon.png" />
+	
+	<title><?php wp_title("&raquo;", true, "right"); ?> Cyklomapa Plzně a okolí - přehled cyklotras a cyklostezek</title>
 	
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
 	<link rel="stylesheet" media="(max-width: 500px)" href="<?php bloginfo('template_url'); ?>/style-500.css" />
